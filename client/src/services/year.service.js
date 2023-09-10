31fc2a75e6a96b9a3a530e6ffaea42b6a1eec80a
b2007220 <127931507+b2007220@users.noreply.github.com>
2023-09-10 23:15:53 +0700

    update

---
+++ client/src/services/year.service.js
@@ -0,0 +1,39 @@
+import createClient from '../utils/client';
+
+class YearService {
+	#client;
+
+	constructor() {
+		this.#client = createClient('Years');
+	}
+
+	async createYear(year) {
+		const newyear = await this.#client.post('/', year);
+
+		return newyear;
+	}
+
+	async getAllYears() {
+		const years = await this.#client.get('/');
+
+		return years;
+	}
+
+	async getYearById(id) {
+		const year = await this.#client.get(`/${id}`);
+
+		return year;
+	}
+
+	async updateYearById(id, year) {
+		const updatedyear = await this.#client.put(`/${id}`, year);
+
+		return updatedyear;
+	}
+
+	async deleteYearById(id) {
+		await this.#client.delete(`/${id}`);
+	}
+}
+
+export default new YearService();
