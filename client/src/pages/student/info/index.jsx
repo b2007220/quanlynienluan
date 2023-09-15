73a8d6cffadc69fe35cf67aa00bbfa8c189b168d
b2007220 <127931507+b2007220@users.noreply.github.com>
2023-09-15 14:43:09 +0700

    feat: info student

--- client/src/pages/student/info/index.jsx
+++ client/src/pages/student/info/index.jsx
@@ -4,13 +4,13 @@ import authService from '../../../services/auth.service';
 import { Select, MenuItem } from '@mui/material';
 import majorService from '../../../services/major.service';
 export default function Info() {
-	const [info, setInfo] = useState([]);
+	const [userinfo, setUserinfo] = useState([]);
 	const [major, setMajor] = useState([]);
 	useEffect(() => {
 		authService
 			.getUserProfile()
 			.then((user) => {
-				setInfo(user);
+				setUserinfo(user);
 				majorService.getAllMajors().then((res) => {
 					setMajor(res);
 				});
@@ -19,76 +19,138 @@ export default function Info() {
 				console.log(error);
 			});
 	}, []);
-
+	const handleSubmit = async () => {
+		try {
+		} catch (error) {
+			console.log(error);
+		}
+	};
 	return (
 		<div className={style.details}>
 			<div className={style.recentOrders}>
 				<div className={style.cardHeader}>
 					<h2>Thông tin cá nhân</h2>
 				</div>
-				{setInfo.map((info) => (
-					<form>
-						<div className={style.row50}>
-							<div className={style.input__box}>
-								<span>Họ tên</span>
-								<input
-									type='text'
-									autoComplete='off'
-									required
-									value={values.fullName}
-									name='fullName'
-								></input>
-							</div>
-							<div className={style.input__box}>
-								<span>Ngành học</span>
-							</div>
-							<Select value={info.major.majorName} name='majorName' displayEmpty>
-								{setMajor.map((major) => (
+				<form>
+					<div className={style.row50}>
+						<div className={style.input__box}>
+							<span>Họ tên</span>
+							<input
+								type='text'
+								autoComplete='off'
+								required
+								value={userinfo.fullName}
+								name='fullName'
+							></input>
+						</div>
+						<div className={style.input__box}>
+							<span>Ngành học</span>
+
+							<Select
+								name='majorName'
+								displayEmpty
+								sx={{
+									padding: '10px',
+									outline: 'none',
+									border: '1px solid var(--black1)',
+									resize: 'none',
+									borderRadius: '12px',
+									marginBottom: '10px',
+									fontSize: '1.1em',
+									height: '37px',
+								}}
+							>
+								{major.map((major) => (
 									<MenuItem key={major.id} value={major.code}>
 										{major.majorName}
 									</MenuItem>
 								))}
 							</Select>
 						</div>
-						<div className={style.row50}>
-							<div className={style.input__box}>
-								<span>Email</span>
-								<input type='email' name='email' value={info.email} readonly required></input>
-							</div>
-							<div className={style.input__box}>
-								<span>MSSV</span>
-								<input
-									type='text'
-									name='studentId'
-									required
-									autoComplete='off'
-									value={info.studentId}
-								></input>
-							</div>
+					</div>
+					<div className={style.row50}>
+						<div className={style.input__box}>
+							<span>Email</span>
+							<input type='email' name='email' value={userinfo.email} disabled required></input>
 						</div>
-						<div className={style.row25}>
-							<div className={style.input__box}>
-								<span>Khóa</span>
-								<input type='number' min='42' name='khoa' value={info} required></input>
-							</div>
+						<div className={style.input__box}>
+							<span>MSSV</span>
+							<input
+								type='text'
+								name='studentId'
+								required
+								autoComplete='off'
+								value={userinfo.studentId}
+							></input>
 						</div>
-						<div className={style.row25}>
-							<div className={style.input__box}>
-								<span>Giới tính</span>
-							</div>
-							<Select value={info.gender} name='gender' displayEmpty>
+					</div>
+					<div className={style.row25}>
+						<div className={style.input__box}>
+							<span>Khóa</span>
+							<input type='number' min='42' name='khoa' value={userinfo.course} required></input>
+						</div>
+					</div>
+					<div className={style.row25}>
+						<div className={style.input__box}>
+							<span>Giới tính</span>
+							<Select
+								value={userinfo.gender}
+								name='gender'
+								displayEmpty
+								sx={{
+									padding: '10px',
+									outline: 'none',
+									border: '1px solid var(--black1)',
+									resize: 'none',
+									borderRadius: '12px',
+									marginBottom: '10px',
+									fontSize: '1.1em',
+									height: '37px',
+								}}
+							>
 								<MenuItem value='MALE'>Nam</MenuItem>
 								<MenuItem value='FEMALE'>Nữ</MenuItem>
 								<MenuItem value='HIDĐEN'>Ẩn</MenuItem>
 							</Select>
 						</div>
-						<div className={style.row100}>
-							<div className={style.input__box}>
-								<button type='submit' value='Cập nhật' onClick={handleSubmit}></button>
-							</div>
+					</div>
+					<div className={style.row100}>
+						<div className={style.input__box}>
+							<input type='submit' value='Cập nhật' onClick={handleSubmit}></input>
 						</div>
-					</form>
-				))}
+					</div>
+				</form>
+			</div>
+			<div className={style.recentOrders}>
+				<div className={style.cardHeader}>
+					<h2>Đổi mật khẩu</h2>
+				</div>
+				<form>
+					<div className={style.row100}>
+						<div className={style.input__box}>
+							<span>Mật khẩu cũ</span>
+							<input type='text' autoComplete='off' required name='oldPassword'></input>
+						</div>
+					</div>
+					<div className={style.row100}>
+						<div className={style.input__box}>
+							<span>Xác nhận mật khẩu cũ</span>
+							<input type='text' autoComplete='off' required name='confirmOldPassword'></input>
+						</div>
+					</div>
+					<div className={style.row100}>
+						<div className={style.input__box}>
+							<span>Mật khẩu mới</span>
+							<input type='text' autoComplete='off' required name='newPassword'></input>
+						</div>
+					</div>
+
+					<div className={style.row100}>
+						<div className={style.input__box}>
+							<input type='submit' value='Cập nhật' onClick={handleSubmit}></input>
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	);
